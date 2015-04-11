f24abe25b7d7e9b8783091b30f340055e89eb624
Sash Zats <sash@zats.io>
2015-04-10 17:24:53 -0700

    Added some tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,40 +6,76 @@
 //  Copyright (c) 2014 Sash Zats. All rights reserved.
 //
 
+#import "WMLNavigationNode.h"
+
+
+@interface ViewController : UIViewController <WMLNavigationParametrizedViewController>
+@property (nonatomic, weak) WMLNavigationNode *navigationNode;
+@end
+
+@implementation ViewController
+@end
+
+
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
+describe(@"WMLNavigationNode", ^{
+    context(@"isRootNode", ^{
+        it(@"should return true if has no parent", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            node.child = [WMLNavigationNode node];
+            expect(node.isRootNode).to.beTruthy();
+        });
         
+        it(@"should be falsy has a parent", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            node.child = [WMLNavigationNode node];
+            expect(node.child.isRootNode).to.beFalsy();
+        });
+    });
+    
+    context(@"root", ^{
+        it(@"should report itself if has no parent", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            node.child = [WMLNavigationNode node];
+            expect(node.root).to.beIdenticalTo(node);
+        });
+
+        it(@"should report its parent if it has no parent", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            node.child = [WMLNavigationNode node];
+            expect(node.child.root).to.beIdenticalTo(node);
+        });
+    });
+
+    context(@"leaf", ^{
+        it(@"should report itself if has no child", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            expect(node.leaf).to.beIdenticalTo(node);
+        });
+        
+        it(@"should report its furthest descent if has children", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            node.child = [WMLNavigationNode node];
+            expect(node.leaf).to.beIdenticalTo(node.child);
+        });
+    });
+    
+    context(@"viewController", ^{
+       it(@"should set view controller's node if conforms WMLNavigationParametrizedViewController", ^{
+           WMLNavigationNode *node = [WMLNavigationNode node];
+           node.viewController = [ViewController new];
+           expect(((ViewController *)node.viewController).navigationNode).to.beIdenticalTo(node);
+       });
+        
+        it(@"should stil works for view controller not conforming WMLNavigationParametrizedViewController", ^{
+            WMLNavigationNode *node = [WMLNavigationNode node];
+            expect(^{
+                node.viewController = [ViewController new];
+            }).notTo.raiseAny();
         });
     });
 });
 
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^AsyncBlock {
-        waitUntil(^(DoneCallback done) {
-            done();
-        });
-    });
-});
 
 SpecEnd
